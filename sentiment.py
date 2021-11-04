5fcac8d256c31b7362703d0b0067d94fd0a20661
Şükran Üsküdar <93452468+sukranuskudar@users.noreply.github.com>
2021-11-04 16:06:26 +0300

    init

---
+++ sentiment.py
@@ -0,0 +1,8 @@
+import random
+import nltk
+from nltk.corpus import movie_reviews
+documents = [(list(movie_reviews.words(fileid)), category)
+              for category in movie_reviews.categories()
+              for fileid in movie_reviews.fileids(category)]
+
+random.shuffle(documents)
